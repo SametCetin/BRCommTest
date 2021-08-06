685dfa4f701497b475c57e177849ffd49e0d4f08
Samet CETIN <sametcetin@yandex.com>
2021-08-06 16:51:31 +0300

    pvi denemeleri yapıldı.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,6 +47,8 @@ namespace BRCommTest
             this.tmrWaitFormLoad = new System.Windows.Forms.Timer(this.components);
             this.tmrVisuUpdate = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -64,18 +66,21 @@ namespace BRCommTest
             this.groupBox1.Controls.Add(this.txtGoToPosAxisX1);
             this.groupBox1.Controls.Add(this.txtActPosAxisX1);
             this.groupBox1.Controls.Add(this.btnJogPlusX1);
-            this.groupBox1.Location = new System.Drawing.Point(329, 12);
+            this.groupBox1.Location = new System.Drawing.Point(439, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(321, 232);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(428, 286);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "AxisX1";
             // 
             // btnStopAxisX1
             // 
-            this.btnStopAxisX1.Location = new System.Drawing.Point(72, 140);
+            this.btnStopAxisX1.Location = new System.Drawing.Point(96, 172);
+            this.btnStopAxisX1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStopAxisX1.Name = "btnStopAxisX1";
-            this.btnStopAxisX1.Size = new System.Drawing.Size(60, 40);
+            this.btnStopAxisX1.Size = new System.Drawing.Size(80, 49);
             this.btnStopAxisX1.TabIndex = 13;
             this.btnStopAxisX1.Text = "STOP";
             this.btnStopAxisX1.UseVisualStyleBackColor = true;
@@ -84,35 +89,39 @@ namespace BRCommTest
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(137, 37);
+            this.label3.Location = new System.Drawing.Point(183, 46);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.Size = new System.Drawing.Size(75, 17);
             this.label3.TabIndex = 12;
             this.label3.Text = "Actuel Pos";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(137, 70);
+            this.label2.Location = new System.Drawing.Point(183, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.Size = new System.Drawing.Size(70, 17);
             this.label2.TabIndex = 11;
             this.label2.Text = "Move Pos";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(137, 102);
+            this.label1.Location = new System.Drawing.Point(183, 126);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.Size = new System.Drawing.Size(49, 17);
             this.label1.TabIndex = 10;
             this.label1.Text = "mm/sn";
             // 
             // btnResetAxisX1
             // 
-            this.btnResetAxisX1.Location = new System.Drawing.Point(250, 186);
+            this.btnResetAxisX1.Location = new System.Drawing.Point(333, 229);
+            this.btnResetAxisX1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnResetAxisX1.Name = "btnResetAxisX1";
-            this.btnResetAxisX1.Size = new System.Drawing.Size(60, 40);
+            this.btnResetAxisX1.Size = new System.Drawing.Size(80, 49);
             this.btnResetAxisX1.TabIndex = 9;
             this.btnResetAxisX1.Text = "RST";
             this.btnResetAxisX1.UseVisualStyleBackColor = true;
@@ -121,9 +130,10 @@ namespace BRCommTest
             // 
             // btnJogMinAxisX1
             // 
-            this.btnJogMinAxisX1.Location = new System.Drawing.Point(6, 186);
+            this.btnJogMinAxisX1.Location = new System.Drawing.Point(8, 229);
+            this.btnJogMinAxisX1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnJogMinAxisX1.Name = "btnJogMinAxisX1";
-            this.btnJogMinAxisX1.Size = new System.Drawing.Size(60, 40);
+            this.btnJogMinAxisX1.Size = new System.Drawing.Size(80, 49);
             this.btnJogMinAxisX1.TabIndex = 8;
             this.btnJogMinAxisX1.Text = "JOG -";
             this.btnJogMinAxisX1.UseVisualStyleBackColor = true;
@@ -134,18 +144,20 @@ namespace BRCommTest
             // 
             this.chkEnabledAxisX1.AutoSize = true;
             this.chkEnabledAxisX1.Enabled = false;
-            this.chkEnabledAxisX1.Location = new System.Drawing.Point(250, 19);
+            this.chkEnabledAxisX1.Location = new System.Drawing.Point(333, 23);
+            this.chkEnabledAxisX1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkEnabledAxisX1.Name = "chkEnabledAxisX1";
-            this.chkEnabledAxisX1.Size = new System.Drawing.Size(68, 17);
+            this.chkEnabledAxisX1.Size = new System.Drawing.Size(85, 21);
             this.chkEnabledAxisX1.TabIndex = 7;
             this.chkEnabledAxisX1.Text = "Powered";
             this.chkEnabledAxisX1.UseVisualStyleBackColor = true;
             // 
             // btnMovePosAxisX1
             // 
-            this.btnMovePosAxisX1.Location = new System.Drawing.Point(6, 140);
+            this.btnMovePosAxisX1.Location = new System.Drawing.Point(8, 172);
+            this.btnMovePosAxisX1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMovePosAxisX1.Name = "btnMovePosAxisX1";
-            this.btnMovePosAxisX1.Size = new System.Drawing.Size(60, 40);
+            this.btnMovePosAxisX1.Size = new System.Drawing.Size(80, 49);
             this.btnMovePosAxisX1.TabIndex = 6;
             this.btnMovePosAxisX1.Text = "MOVE";
             this.btnMovePosAxisX1.UseVisualStyleBackColor = true;
@@ -154,9 +166,10 @@ namespace BRCommTest
             // txtMoveVeloAxisX1
             // 
             this.txtMoveVeloAxisX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.txtMoveVeloAxisX1.Location = new System.Drawing.Point(16, 94);
+            this.txtMoveVeloAxisX1.Location = new System.Drawing.Point(21, 116);
+            this.txtMoveVeloAxisX1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMoveVeloAxisX1.Name = "txtMoveVeloAxisX1";
-            this.txtMoveVeloAxisX1.Size = new System.Drawing.Size(115, 26);
+            this.txtMoveVeloAxisX1.Size = new System.Drawing.Size(152, 30);
             this.txtMoveVeloAxisX1.TabIndex = 3;
             this.txtMoveVeloAxisX1.Text = "10";
             this.txtMoveVeloAxisX1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -164,9 +177,10 @@ namespace BRCommTest
             // txtGoToPosAxisX1
             // 
             this.txtGoToPosAxisX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.txtGoToPosAxisX1.Location = new System.Drawing.Point(16, 62);
+            this.txtGoToPosAxisX1.Location = new System.Drawing.Point(21, 76);
+            this.txtGoToPosAxisX1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtGoToPosAxisX1.Name = "txtGoToPosAxisX1";
-            this.txtGoToPosAxisX1.Size = new System.Drawing.Size(115, 26);
+            this.txtGoToPosAxisX1.Size = new System.Drawing.Size(152, 30);
             this.txtGoToPosAxisX1.TabIndex = 2;
             this.txtGoToPosAxisX1.Text = "100";
             this.txtGoToPosAxisX1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -174,18 +188,20 @@ namespace BRCommTest
             // txtActPosAxisX1
             // 
             this.txtActPosAxisX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtActPosAxisX1.Location = new System.Drawing.Point(16, 29);
+            this.txtActPosAxisX1.Location = new System.Drawing.Point(21, 36);
+            this.txtActPosAxisX1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtActPosAxisX1.Name = "txtActPosAxisX1";
             this.txtActPosAxisX1.ReadOnly = true;
-            this.txtActPosAxisX1.Size = new System.Drawing.Size(115, 26);
+            this.txtActPosAxisX1.Size = new System.Drawing.Size(152, 30);
             this.txtActPosAxisX1.TabIndex = 1;
             this.txtActPosAxisX1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // btnJogPlusX1
             // 
-            this.btnJogPlusX1.Location = new System.Drawing.Point(71, 186);
+            this.btnJogPlusX1.Location = new System.Drawing.Point(95, 229);
+            this.btnJogPlusX1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnJogPlusX1.Name = "btnJogPlusX1";
-            this.btnJogPlusX1.Size = new System.Drawing.Size(60, 40);
+            this.btnJogPlusX1.Size = new System.Drawing.Size(80, 49);
             this.btnJogPlusX1.TabIndex = 0;
             this.btnJogPlusX1.Text = "JOG +";
             this.btnJogPlusX1.UseVisualStyleBackColor = true;
@@ -195,9 +211,10 @@ namespace BRCommTest
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(13, 425);
+            this.lblStatus.Location = new System.Drawing.Point(17, 523);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(29, 13);
+            this.lblStatus.Size = new System.Drawing.Size(40, 17);
             this.lblStatus.TabIndex = 5;
             this.lblStatus.Text = "Error";
             // 
@@ -212,24 +229,47 @@ namespace BRCommTest
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(61, 72);
+            this.button1.Location = new System.Drawing.Point(61, 141);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 6;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "BRPlcVar1";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -256,6 +296,8 @@ namespace BRCommTest
         private System.Windows.Forms.Button btnStopAxisX1;
         private System.Windows.Forms.Timer tmrVisuUpdate;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
-
+using BR.AN.PviServices;
 
 namespace BRCommTest
 {
@@ -24,7 +24,7 @@ namespace BRCommTest
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            tmrWaitFormLoad.Start();
+            //tmrWaitFormLoad.Start();
         }
 
         private void tmrWaitFormLoad_Tick(object sender, EventArgs e)
@@ -92,11 +92,7 @@ namespace BRCommTest
             PlcComm.Vars.HMI.Axis[0].Reset = false;
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Form2 frm = new Form2(this);
-            frm.Show();
-        }
+        
 
         private void btnMovePosAxisX1_Click(object sender, EventArgs e)
         {
@@ -108,6 +104,77 @@ namespace BRCommTest
         private void btnStopAxisX1_Click(object sender, EventArgs e)
         {
             PlcComm.Vars.HMI.Axis[0].MoveSetPos = false;
+        }
+
+        
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (BRPlcVar1 != null)
+                BRPlcVar1.Disconnect();
+
+            if (BRCpu1 != null)
+                BRCpu1.Disconnect();
+
+            if (BRService1 != null)
+                BRService1.Disconnect();
+        }
+
+        private BR.AN.PviServices.Service BRService1;
+        private BR.AN.PviServices.Cpu BRCpu1;
+        private BR.AN.PviServices.Variable BRPlcVar1;
+
+        private string DestAddress1 = "127.0.0.1";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Form2 frm = new Form2(this);
+            //frm.Show();
+            ConnectToPlc();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (BRCpu1 != null && BRCpu1.IsConnected)
+            {
+                if (this.BRPlcVar1 == null)
+                {
+                    BRPlcVar1 = new BR.AN.PviServices.Variable(BRCpu1, textBox1.Text);
+                    BRPlcVar1.ValueRead += new PviEventHandler(BRPlcVar1_ValueRead);
+                }
+                BRPlcVar1.Connect();
+                BRPlcVar1.ReadValue();                
+            }
+            else
+                MessageBox.Show("Connection fault");
+        }
+
+
+        private void ConnectToPlc()
+        {
+            BRService1 = new Service("BRService1");
+            BRService1.Connect();
+            BRService1.Connected += new PviEventHandler(BRService1_Connected);
+        }
+
+        private void BRService1_Connected(object sender, PviEventArgs e)
+        {
+            BRCpu1 = new BR.AN.PviServices.Cpu(BRService1, "BRCpu1");
+            BRCpu1.Connection.DeviceType = DeviceType.TcpIp;
+            BRCpu1.Connection.TcpIp.DestinationIpAddress = "127.0.0.1";
+
+            BRCpu1.Connect();
+            BRCpu1.Connected += new PviEventHandler(BRCpu1_Connected);
+        }
+
+        private void BRCpu1_Connected(object sender, PviEventArgs e)
+        {
+            lblStatus.Text = "connectedCpu";
+        }
+
+        private void BRPlcVar1_ValueRead(object sender, PviEventArgs e)
+        {
+            MessageBox.Show(((Variable)sender).Value.ToString());
         }
     }
 }
